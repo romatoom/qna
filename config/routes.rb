3b9c5676a807a6c0cd369a88ea7f2718b84681bf
Roman Ivanov <romani@epayservices.com>
2022-11-01 16:39:17 +0500

    improve: views, add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :questions, only: %i(new create show) do
+  root 'questions#index'
+
+  resources :questions, only: %i(new create show index) do
     resources :answers, only: %i(new create show)
   end
 end
