b5b83b781a8053bb0ef84fc117a20cee39aa655d
Roman Ivanov <romani@epayservices.com>
2022-11-01 16:37:28 +0500

    add: test GET #index for questions

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -50,4 +50,18 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :show
     end
   end
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 3) }
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to eq questions
+    end
+
+    it 'render index view' do
+      expect(response).to render_template :index
+    end
+  end
 end
