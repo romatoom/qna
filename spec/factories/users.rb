844adc354fee33cdd4a21818161c3606c149d760
Roman Ivanov <romani@epayservices.com>
2022-11-06 15:55:12 +0500

    add: tests for sign in, update users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :user do
+    email { "user@test.com" }
+    password { "12345678" }
+
+    trait :wrong_email do
+      email { "wrong_user@test.com" }
+    end
   end
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,16 +5,27 @@ feature 'User can sign in', %q(
   As an unauthenticated user
   I'd like to be able to sign in
 ) do
+
+  given(:user) { create(:user) }
+  given(:wrong_user) { OpenStruct.new(attributes_for(:user, :wrong_email)) }
+
+  given(:login_at_some_user) do
+    lambda do |user|
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Log in'
+    end
+  end
+
+  background { visit new_user_session_path }
+
   scenario 'Registered user tries to sign in' do
-    User.create!(email: 'user@test.com', password: '12345678')
-
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-
-    save_and_open_page
+    login_at_some_user.call(user)
     expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sign in'
+  scenario 'Unregistered user tries to sign in' do
+    login_at_some_user.call(wrong_user)
+    expect(page).to have_content 'Invalid Email or password.'
+  end
 end
