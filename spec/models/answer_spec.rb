50c6e35a652d70fa088fd7a7af00b85ce008cef7
Roman Ivanov <romani@epayservices.com>
2022-11-08 21:28:23 +0500

    improve: implemented story "Author can delete his answer, but cannot delete someone else's answer" (with tests), refactor

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to(:question) }
+  it { should belong_to(:author).class_name('User').with_foreign_key('author_id') }
 
   it { should validate_presence_of(:question) }
   it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:author) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:users_answers).dependent(:destroy) }
+  it { should have_many(:answers).through(:users_answers) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 end
